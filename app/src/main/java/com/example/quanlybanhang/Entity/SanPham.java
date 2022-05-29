8a5098ae761fa5c0d94d129e7dba3cec5d4495c6
leducthang2011 <lthang058@gmail.com>
2022-05-29 15:02:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quanlybanhang/Entity/SanPham.java
@@ -0,0 +1,64 @@
+package com.example.quanlybanhang.Entity;
+
+public class SanPham {
+    private int id;
+    private String tenSP, loaiSP;
+    private int giaBan, soLuong;
+
+    public SanPham() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public String getLoaiSP() {
+        return loaiSP;
+    }
+
+    public void setLoaiSP(String loaiSP) {
+        this.loaiSP = loaiSP;
+    }
+
+    public int getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(int giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public SanPham(int id, String tenSP, String loaiSP, int giaBan, int soLuong) {
+        this.id = id;
+        this.tenSP = tenSP;
+        this.loaiSP = loaiSP;
+        this.giaBan = giaBan;
+        this.soLuong = soLuong;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(" Tên sản phẩm: %s \n Loại sản phẩm: %s \n Giá bán: %s VNĐ \n Số lượng: %s", tenSP, loaiSP, giaBan, soLuong);
+    }
+}
